4ccb26eb193cc787e5603dd0af910487b940cde6
Irene_home <puigparnaui@gmail.com>
2024-12-09 09:48:48 +0100

    starting parser

---
+++ parser.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ipuig-pa <ipuig-pa@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/08 12:49:53 by ipuig-pa          #+#    #+#             */
+/*   Updated: 2024/12/08 12:49:53 by ipuig-pa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.c"
+
+//TO MOVE TO HEADER FILE
+typedef struct s_astnode
+{
+	t_token		token;
+	t_astnode	*right_node;
+	t_astnode	*left_node;
+} t_astnode
+
+
+//parser (syntactic analysis): builds an Abstract Syntax Tree (AST) using recursive descent parsing, and returns a pointer to AST root. The AST has the tokens as nodes, already correctly classified, and hierarchized.
+t_astnode	*parse (t_token *tokens)
+{
+	t_astnode root;
+
+	root =
+
+	return (root);
+}
