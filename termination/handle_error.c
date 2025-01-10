9d041967f7188574293615d05eddc9089a155522
Erya Wu <ewu@2-G-2.42heilbronn.de>
2025-01-10 12:49:55 +0100

    handle error

--- termination/handle_error.c
+++ termination/handle_error.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   handle_error.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ipuig-pa <ipuig-pa@student.42heilbronn.    +#+  +:+       +#+        */
+/*   By: ewu <ewu@student.42heilbronn.de>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/31 10:53:18 by ipuig-pa          #+#    #+#             */
-/*   Updated: 2024/12/31 10:53:18 by ipuig-pa         ###   ########.fr       */
+/*   Updated: 2025/01/10 11:18:18 by ewu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,35 @@ void	handle_error(t_gc_list *gc_list)
 	gc_clean(gc_list);
 	//or exit or something like this
 	perror ("Heap allocation fail\n");
+}
+
+void print_err(char *s1, char *s2, char *s3)
+{
+	ft_putstr_fd(s1, 2);
+	ft_putstr_fd(": ", 2);
+	if (s2)
+	{
+		ft_putstr_fd(s2, 2);
+		ft_putstr_fd(": ", 2);
+	}
+	if (s3)
+	{
+		ft_putstr_fd(s3, 2);
+		ft_putstr_fd("\n", 2);
+	}
+}
+
+void free_env(char **env)
+{
+	int i;
+
+	if (env == NULL)
+		return ;
+	i = 0;
+	while (env[i])
+	{
+		free(env[i]);
+		i++;
+	}
+	free(env);
 }
