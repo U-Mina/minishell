810233088e0682c5b459a20bd179c91b5f31cc5e
Erya Wu <ewu@2-G-2.42heilbronn.de>
2025-01-10 13:54:44 +0100

    1011 todo: - redirection (handler needed: for fd_error checking and clean) - init and pass of var for pipe/redir/ (!!!important!!)

--- execution/redirect.c
+++ execution/redirect.c
@@ -3,54 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   redirect.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ipuig-pa <ipuig-pa@student.42heilbronn.    +#+  +:+       +#+        */
+/*   By: ewu <ewu@student.42heilbronn.de>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/05 20:28:11 by ewu               #+#    #+#             */
-/*   Updated: 2025/01/09 12:30:02 by ipuig-pa         ###   ########.fr       */
+/*   Updated: 2025/01/10 13:52:00 by ewu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../builtins/minishell.h"
 
-//todo: 0105晚：增加了t_redir结构（包含fd和flag，引用t_astnode结构）
-
-/**
- * typedef enum e_tokentype
-{
-	REDIRECTION,
-	PIPE,
-	INPUT,
-	OUTPUT,
-	FILENAME,
-	HEREDOC,
-	APPEND
-}						t_tokentype;
-
-typedef struct s_token
-{
-	t_tokentype			type;
-	char				*value;
-}						t_token;
-
-typedef struct s_astnode
-{
-	int fd[2];
-	t_token				*token;
-	struct s_astnode	*right;
-	struct s_astnode	*left;
-	struct s_astnode	*next_arg;
-	t_redir				*redirect
-}						t_astnode;
-
-typedef struct s_redir
-{
-	int					fd;
-	t_token				*token;
-	t_redir			*right;
-	t_redir			*left;
-
-}						t_redir;
- */
+//todo: ft handle_redir(), this ft will do error_check for fd(<0?) and clean and direct to exec_redir
 
 /* redirection_node: retval of @fn: t_astnode *parse_redirection*/
 int exec_redirect(t_astnode *astnode, int *exit_status)
