57f7270bb2f7e2f84a7d6f2937c5b08ca2b66c79
Irene Puig Parnau <ipuig-pa@3-H-3.42heilbronn.de>
2025-01-11 17:06:30 +0100

    garbage collector reorg

--- termination/handle_error.c
+++ termination/handle_error.c
@@ -3,22 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   handle_error.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ewu <ewu@student.42heilbronn.de>           +#+  +:+       +#+        */
+/*   By: ipuig-pa <ipuig-pa@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/31 10:53:18 by ipuig-pa          #+#    #+#             */
-/*   Updated: 2025/01/10 12:51:17 by ewu              ###   ########.fr       */
+/*   Updated: 2025/01/11 17:05:46 by ipuig-pa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-void	*handle_error(t_gc_list *gc_list)
-{
-	gc_clean(gc_list);
-	//or exit or something like this
-	perror ("Heap allocation fail\n");
-	return (NULL);
-}
 
 void print_err(char *s1, char *s2, char *s3)
 {
