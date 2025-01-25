5ec6f85b80033c202a147565820f4fa0cf9c3968
Erya Wu <ewu@2-G-2.42heilbronn.de>
2025-01-25 14:46:55 +0100

    fix over 25 lines issue in ft_exit()

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ipuig-pa <ipuig-pa@student.42heilbronn.    +#+  +:+       +#+        */
+/*   By: ewu <ewu@student.42heilbronn.de>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 16:19:34 by ewu               #+#    #+#             */
-/*   Updated: 2025/01/21 20:50:43 by ipuig-pa         ###   ########.fr       */
+/*   Updated: 2025/01/25 14:44:58 by ewu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,33 +32,30 @@ static int	is_digit(char *s)
 void ft_exit(t_data *data, char **args)
 {
 	printf("exit\n");
-	if (args_nbr(args) > 1)
+	if (args_nbr(args) > 2)
 	{
-		if (args_nbr(args) == 2)
+		print_err("minishell", "exit", "too many arguments");
+		data->exit_status = 1;
+		return ;
+	}
+	if (args_nbr(args) == 2)
+	{
+		if (is_digit(args[1]) == 0)
 		{
-			if (is_digit(args[1]) == 0)
-			{
-				data->exit_status = ft_atoi(args[1]);
-				term_minishell(data->minishell, ft_atoi(args[1]));
-			}
-			else if (is_digit(args[1]) == -1)
-			{
-				print_err("minishell", args[1], "numeric argument required" );
-				data->exit_status = 255;
-				term_minishell(data->minishell, 255);
-			}
+			data->exit_status = ft_atoi(args[1]);
+			term_minishell(data->minishell, ft_atoi(args[1]));
 		}
-		else
+		else if (is_digit(args[1]) == -1)
 		{
-			print_err("minishell", "exit", "too many arguments" );
-			data->exit_status = 1;
-			return ;
+			print_err("minishell", args[1], "numeric argument required");
+			data->exit_status = 255;
+			term_minishell(data->minishell, 255);
 		}
 	}
 	data->exit_status = 0;
 	term_minishell(data->minishell, 0);
 }
-
+		
 // void ft_exit(char **args, int *exit_status)
 // {
 // 	printf("exit\n");
